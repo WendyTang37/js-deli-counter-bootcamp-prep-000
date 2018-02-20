db18f78b1b7b86c41c4ff8904ed0c722a4a29cee
Wendy Tang <wenhuitang0521@gmail.com>
2018-02-20 02:58:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line) {
   if(line.length === 0) {
     return "The line is currently empty."
   }
-  var lineString = "The line is currently: ";
+  var lineString = "The line is currently:";
   for(let i = 0; i < line.length; i++) {
     lineString = lineString + `${i===0?'':','} ${i+1}. ${line[i]}`;
   }
