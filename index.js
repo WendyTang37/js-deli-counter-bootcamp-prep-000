2a338cfb6b4ff450bf23b3127446f29c6b8b8219
Wendy Tang <wenhuitang0521@gmail.com>
2018-02-20 02:47:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, newCustomer) {
   var numOfPeopleInLine = katzDeliLine.length;
+  katzDeliLine.push(newCustomer);
   return numOfPeopleInLine + 1;
 }
 
