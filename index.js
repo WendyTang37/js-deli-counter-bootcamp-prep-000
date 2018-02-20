4408c7d9eedb8d64eafeb2846a1696e84db15d59
Wendy Tang <wenhuitang0521@gmail.com>
2018-02-20 02:52:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!";
   } else {
     var nextCustomer = katzDeliLine.shift();
-    return `Currently serving ${nextCustomer}`;
+    return `Currently serving ${nextCustomer}.`;
   }
 }
